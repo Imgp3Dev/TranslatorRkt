1c725d076a1e9df1e97375a4c26d50125c62c4d2
IMGP3DEV <42507604+Imgp3Dev@users.noreply.github.com>
2018-11-02 02:53:59 -0500

    Create Translator.rkt

---
+++ source/Translator.rkt
@@ -0,0 +1,13 @@
+#lang racket
+;;Racket Translator
+#| Supports: Japanese, Chinese, Spanish,
+English #|
+(list (languages `(English, Chinese,
+Japanese, Spanish)))
+if (Languages? Input
+(Languages(input)))
+if(Languages? No-input
+("Please select a language"))
+(define(Languages(input))'()
+;;Enter optional language then enter a msg
+;;Example: japanese Hi
